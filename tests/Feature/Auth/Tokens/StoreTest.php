045954c2720233d3a8520a28e19ec73031d33888
DengSihan <root@dsh.email>
2023-01-10 14:49:19 +0000

    test: store tokens failed

--- tests/Feature/Auth/Tokens/StoreTest.php
+++ tests/Feature/Auth/Tokens/StoreTest.php
@@ -32,6 +32,45 @@ class StoreTest extends TestCase
                     'created_at',
                     'updated_at',
                 ],
+            ])
+            ->assertJson([
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'email_verified_at' => $user->email_verified_at->jsonSerialize(),
+                    'created_at' => $user->created_at->jsonSerialize(),
+                    'updated_at' => $user->updated_at->jsonSerialize(),
+                ],
+            ]);
+    }
+
+    public function test_store_tokens_failed_response () {
+
+        $user = User::factory()->create();
+
+        $response = $this->withHeaders([
+                'User-Agent' => $this->faker->userAgent,
+            ])
+            ->postJson($this->api, [
+                'email' => $user->email,
+                'password' => 'wrong-password',
+            ]);
+
+        $response->assertStatus(401)
+            ->assertJsonStructure([
+                'message',
+                'errors' => [
+                    'email',
+                ]
+            ])
+            ->assertJson([
+                'message' => __('auth.failed'),
+                'errors' => [
+                    'email' => [
+                        __('auth.failed'),
+                    ],
+                ],
             ]);
     }
 }
