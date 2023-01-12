1385acc47a32cf5b71beea0e77dcf0eb0c5cc501
DengSihan <root@dsh.email>
2023-01-12 15:47:35 +0000

    refactor: remove bind social account while authed

--- app/Http/Controllers/Auth/SocialTokensController.php
+++ app/Http/Controllers/Auth/SocialTokensController.php
@@ -24,60 +24,35 @@ class SocialTokensController extends Controller
 
         $credentials = Socialite::driver($type)->stateless()->user();
         
-        // if the user is logged in here, this will bind the social account to the user 
-        if ($user = $request->user()) {
+        $user = User::where('social->' . $type, '=', $credentials->id)->first();
 
-            // if the social account has been binded to another account, remove it
-            if (
-                $exist = User::where([
-                        ['social->'.$type, '=', $credentials->id],
-                        ['id', '<>', $user->id]
-                    ])
-                    ->first()
-            ) {
-                $exist_social = $exist->social;
-                unset($exist_social[$type]);
-                $exist->social = $exist_social;
-                $exist->save();
-            }
-
-            $social = $user->social;
-            $social[$type] = $credentials->id;
-            $user->social = $social;
-            $user->save();
-
-            return redirect($this->getProfilePageUrl(), 302);
+        // if the user is not exist, create a new one
+        if (!$user) {
+            $profile = [
+                'name' => 'user-' . Str::random(8),
+                'social' => [
+                    $type => $credentials->id
+                ]
+            ];
+            $user = User::create($profile);
         }
-        else {
-            $user = User::where('social->' . $type, '=', $credentials->id)->first();
 
-            // if the user is not exist, create a new one
-            if (!$user) {
-                $profile = [
-                    'name' => 'user-' . Str::random(8),
-                    'social' => [
-                        $type => $credentials->id
-                    ]
-                ];
-                $user = User::create($profile);
-            }
+        $token = $user->createToken(Str::random(8))->plainTextToken;
 
-            $token = $user->createToken(Str::random(8))->plainTextToken;
+        // https://github.com/laravel/framework/blob/f02eca97368f4973079f279955cb3ed2551a985b/src/Illuminate/Foundation/helpers.php#L308
+        $cookie = cookie(
+            'token',
+            $token,
+            config('auth.validity_period'),
+            null,
+            config('app.domain'),
+            null,
+            false,
+            true,
+        );
 
-            // https://github.com/laravel/framework/blob/f02eca97368f4973079f279955cb3ed2551a985b/src/Illuminate/Foundation/helpers.php#L308
-            $cookie = cookie(
-                'token',
-                $token,
-                config('auth.validity_period'),
-                null,
-                config('app.domain'),
-                null,
-                false,
-                true,
-            );
-
-            return redirect($this->getProfilePageUrl(), 302)
-                ->withCookie($cookie);
-        }
+        return redirect($this->getProfilePageUrl(), 302)
+            ->withCookie($cookie);
+        
     }
 }
--- routes/api/auth/tokens.php
+++ routes/api/auth/tokens.php
@@ -12,6 +12,18 @@ Route::group([
             'TokensController@store'
         )
         ->name('tokens.store');
+
+    $socials = implode('|', array_diff(array_keys(config('services')), ['mailgun', 'postmark', 'ses']));
+
+    // social login
+    Route::post('social/{type}/tokens', 'SocialTokensController@redirect')
+        ->where('type', $socials)
+        ->name('social.tokens.redirect');
+    
+    // social login callback
+    Route::any('social/{type}/tokens/callback', 'SocialTokensController@callback')
+        ->where('type', $socials)
+        ->name('social.tokens.callback');
         
 });
 
@@ -29,16 +41,3 @@ Route::group([
         ->name('tokens.destroyCurrent');
     
 });
-
-
-$socials = implode('|', array_diff(array_keys(config('services')), ['mailgun', 'postmark', 'ses']));
-
-// social login
-Route::post('social/{type}/tokens', 'SocialTokensController@redirect')
-    ->where('type', $socials)
-    ->name('social.tokens.redirect');
-
-// social login callback
-Route::any('social/{type}/tokens/callback', 'SocialTokensController@callback')
-    ->where('type', $socials)
-    ->name('social.tokens.callback');
